4a927663c433f4a58fb54447dcf3345efe1e96da
[Abdulraof-Mattur] <[abdulraof.y.mattur@gmail.com]>
2023-07-21 16:03:30 +0300

    Abdulraof

--- frontend/src/Components/LandingPage/OurTeamContainer/ourTeamContainer.jsx
+++ frontend/src/Components/LandingPage/OurTeamContainer/ourTeamContainer.jsx
@@ -12,7 +12,7 @@ export default function ourTeamContainer() {
           width: "93%",
         }}
       >
-        <OurTeam img="./image/Abed.png" name="Abdulraof Mattur" />
+        <OurTeam img="./image/Abdulraof.jpg" name="Abdulraof Mattur" />
         <OurTeam img="./image/duha.png" name="Duha Jilani" />
         <OurTeam img="./image/jehad.jpg" name="Jehad Mansra" />
         <OurTeam img="./image/Kayyali.jpg" name="Mohammad Alkayyali" />
