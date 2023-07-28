9d186b950f1bc254ae183c060d9d18467d87dac9
[Abdulraof-Mattur] <[abdulraof.y.mattur@gmail.com]>
2023-07-28 16:34:40 +0300

    abed

--- frontend/src/Components/tourCard/tourCard.jsx
+++ frontend/src/Components/tourCard/tourCard.jsx
@@ -1,32 +1,37 @@
-// import React from "react";
-// import "./tourCard.css";
+import React from "react";
+import "./tourCard.css";
+import { useNavigate } from "react-router-dom";
+function TourCard({ image, price, name, duration, id }) {
+  const navigate = useNavigate();
+  console.log(id);
+  return (
+    <>
+      <div
+        className="discover-card"
+        onClick={() => navigate(`/TourList/TourDetails/${id}`)}
+      >
+        <img src={image} alt="" />
+        <h2 className="dicover-title-card">{name}</h2>
 
-// function TourCard({ image, price, name, duration }) {
-//   return (
-//     <>
-//       <div className="discover-card">
-//         <img src={image} alt="" />
-//         <h2 className="dicover-title-card">{name}</h2>
+        <div className="discover-card-details">
+          <h3 className="price">{price} JD</h3>
+          <div className="icon-days">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="currentColor"
+              className="bi bi-cursor-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z" />
+            </svg>
+            <p>{duration} days for trips</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-//         <div className="discover-card-details">
-//           <h3 className="price">{price} JD</h3>
-//           <div className="icon-days">
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="18"
-//               height="18"
-//               fill="currentColor"
-//               className="bi bi-cursor-fill"
-//               viewBox="0 0 16 16"
-//             >
-//               <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z" />
-//             </svg>
-//             <p>{duration} days for trips</p>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default TourCard;
+export default TourCard;
